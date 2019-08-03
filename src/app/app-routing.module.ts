20e09fb3656aaa832c2bdfbf4fbf395538836816
Darwin Felipe Uzcategui Gonzalez <Darwin.Uzcategui1973@hotmail.com>
2019-08-03 11:35:38 -0400

    Fin sección 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './paginas/portafolio/portafolio.component';
+import { AboutComponent } from './paginas/about/about.component';
+import { ItemComponent } from './paginas/item/item.component';
 
 
-const routes: Routes = [];
+
+const rutas: Routes =[
+  {path: 'inicio', component: PortafolioComponent },
+  {path: 'about', component: AboutComponent },
+  {path: 'item', component: ItemComponent },
+  {path: '**', pathMatch: 'full', redirectTo: 'inicio' }
+
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(rutas, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+// import { AppRoutingModule } from './app-routing.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './paginas/portafolio/portafolio.component';
+import { AboutComponent } from './paginas/about/about.component';
+import { ItemComponent } from './paginas/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
